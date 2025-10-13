44530fbee4ee5de25d3008a38b0c29a2578293e3
leo-reche <ladreche@gmail.com>
2025-10-13 12:53:45 +0200

    Initial commit

---
+++ forward-chatbot.js
@@ -0,0 +1,16 @@
+
+// Event listener for clicks on ChatBot links
+document.addEventListener('click', function(event) {
+
+  const link = event.target.closest('a'); // This line finds the closest link element
+
+  if (link && link.target === '_blank') { //If the link is to a redirected page & 
+    const pxeDiv = event.target.closest('.pxe-markdown');  //There is a pxe ancestor
+    if (pxeDiv) {
+        fetch('https://hooks.zapier.com/hooks/catch/8011346/u9p26zt/', {
+            method: 'POST',
+            body: JSON.stringify({ datafield: link.href }),
+        }); //Records a click
+    }
+  }
+});
